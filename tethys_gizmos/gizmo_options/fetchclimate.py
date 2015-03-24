47b0a1c715989bab34e6731f90693467b25174e9
ezrajr <ezrajrice@gmail.com>
2015-03-24 13:12:08 -0600

    #28 finished  google_map and working on fetchclimate

--- tethys_gizmos/gizmo_options/fetchclimate.py
+++ tethys_gizmos/gizmo_options/fetchclimate.py
@@ -1,13 +1,16 @@
 from .base import TethysGizmoOptions
 
-__all__ = ['URLParameter', 'MapParameters', 'MapData', 'PlotParameters', 'VariableParameters', 'GridParameters', 'PointParameters']
+__all__ = ['FetchClimateMap', 'FetchClimateURLParameter', 'FetchClimateMapData', 'FetchClimateMapParameters',
+           'FetchClimatePlotParameters', 'FetchClimateVariableParameters', 'FetchClimateGridParameters',
+           'FetchClimatePointParameters']
 
-
-class URLParameter(TethysGizmoOptions):
+class FetchClimateURLParameter(TethysGizmoOptions):
     """
     URL Parameter
 
-    This gizmo can be used to get climate data based off of a bounding box over an area or a point. It is based off of Microsoft Research. See the `FetchClimate page reference <http://research.microsoft.com/en-us/projects/fetchclimate/>`_ for more info.
+    This gizmo can be used to get climate data based off of a bounding box over an area or a point.
+    It is based off of Microsoft Research. See the `FetchClimate page reference
+    <http://research.microsoft.com/en-us/projects/fetchclimate/>`_ for more info.
 
     Attributes
     serverUrl(string): The URL to the FetchClimate server (e.g 'serverUrl':'http://fetchclimate2.cloudapp.net')
@@ -18,40 +21,21 @@ class URLParameter(TethysGizmoOptions):
         Constructor
         """
         # Initialize super class
-        super(URLParameter, self).__init__()
+        super(TethysGizmoOptions, self).__init__()
 
         self.serverUrl = serverUrl
 
 
-class MapParameters(TethysGizmoOptions):
-    """
-    Map Parameters
-    Optional if grid or point included. Otherwise, required!
-
-    Attributes
-    css(dictionary):Custom css elements. FORMAT:{'css-element-name': 'css-value'}. If no width or height included, 500px X 500px assumed.
-    map_data(dictionary): Data needed to create the map.
-    """
-
-    def __init__(self, css={}, map_data={}):
-        """
-        Constructor
-        """
-        # Initialize super class
-        super(MapParameters, self).__init__()
-
-        self.css = css
-        self.map_data = map_data
-
-
-class MapData(TethysGizmoOptions):
+class FetchClimateMapData(TethysGizmoOptions):
     """
     Map Parameters - map_data
 
     Attributes
     api_key(string): API key for Google maps.
-    drawing_types_enabled(string array): A list of the types of geometries the user will be allowed to draw. Valid types are: RECTANGLE, and POINTS. (e.g.: drawing_types_enabled=['RECTANGLE','POINTS'])
-    initial_drawing_mode(string array): A string representing the drawing mode that will be enabled by default. Valid modes are: 'RECTANGLE', 'POINTS'. The mode used must be one of the drawing_types_enabled that the user is allowed to draw.
+    drawing_types_enabled(string array): A list of the types of geometries the user will be allowed to draw.
+    Valid types are: RECTANGLE, and POINTS. (e.g.: drawing_types_enabled=['RECTANGLE','POINTS'])
+    initial_drawing_mode(string array): A string representing the drawing mode that will be enabled by default.
+    Valid modes are: 'RECTANGLE', 'POINTS'. The mode used must be one of the drawing_types_enabled that the user is allowed to draw.
     max_num_grids(integer): The maximum number of grids allowed for the user. Default is unlimited. (e.g. 'max_num_grids':0).
     max_num_points(integer): The maximum number of points allowed for the user. Default is unlimited. (e.g 'max_num_points':0).
     """
@@ -61,7 +45,7 @@ class MapData(TethysGizmoOptions):
         Constructor
         """
         # Initialize super class
-        super(MapData, self).__init__()
+        super(TethysGizmoOptions, self).__init__()
 
         self.api_key = api_key
         self.drawing_types_enabled = drawing_types_enabled
@@ -69,30 +53,55 @@ class MapData(TethysGizmoOptions):
         self.max_num_grids = max_num_grids
         self.max_num_points = max_num_points
 
-
-class PlotParameters(TethysGizmoOptions):
+class FetchClimateMapParameters(TethysGizmoOptions):
     """
-    Plot Parameters
+    Map Parameters
+    Optional if grid or point included. Otherwise, required!
 
     Attributes
-    dimensions(string dictionary): The integer is in pixels for width (`Highcharts width reference <http://api.highcharts.com/highcharts#chart.width>`_) or height (`Highcharts height reference <http://api.highcharts.com/highcharts#chart.height>`_). Not required to be defined.
+    css(dictionary):Custom css elements. FORMAT:{'css-element-name': 'css-value'}.
+    If no width or height included, 500px X 500px assumed.
+    map_data(dictionary): Data needed to create the map.
     """
 
-    def __init__(self, dimensions={'width':'100%', 'height':'500px'}):
+    def __init__(self, css={}, map_data=FetchClimateMapData()):
         """
         Constructor
         """
         # Initialize super class
-        super(PlotParameters, self).__init__()
+        super(TethysGizmoOptions, self).__init__()
+
+        self.css = css
+        self.map_data = map_data
+
+class FetchClimatePlotParameters(TethysGizmoOptions):
+    """
+    Plot Parameters
+
+    Attributes
+    dimensions(string dictionary): The integer is in pixels for width (`Highcharts width reference
+    <http://api.highcharts.com/highcharts#chart.width>`_) or height (`Highcharts height reference
+    <http://api.highcharts.com/highcharts#chart.height>`_). Not required to be defined.
+    """
+
+    def __init__(self, dimensions={'width': 100, 'height': 500}):
+        """
+        Constructor
+        """
+        # Initialize super class
+        super(TethysGizmoOptions, self).__init__()
 
         self.dimensions = dimensions
 
 
-class VariableParameters(TethysGizmoOptions):
+class FetchClimateVariableParameters(TethysGizmoOptions):
     """
     Variable Parameters
 
-    To find out which variables you can use and their parameters, go to your service url with '/api/coniguration' at the end. (e.g. `http://fetchclimate2.cloudapp.net/api/configuration <http://fetchclimate2.cloudapp.net/api/configuration>`_). Look in "EnvironmentalVariables" for the variable names. Then, to find the data source ID's of sources available, go to "DataSources".
+    To find out which variables you can use and their parameters, go to your service url
+    with '/api/coniguration' at the end. (e.g. `http://fetchclimate2.cloudapp.net/api/configuration
+    <http://fetchclimate2.cloudapp.net/api/configuration>`_). Look in "EnvironmentalVariables" for the
+    variable names. Then, to find the data source ID's of sources available, go to "DataSources".
 
     Attributes
     variables(dictionary): Must have variable defined. It is in the format {'variable_name':[variable_id,variable_id,variable_id]}.
@@ -103,12 +112,12 @@ class VariableParameters(TethysGizmoOptions):
         Constructor
         """
         # Initialize super class
-        super(VariableParameters, self).__init__()
+        super(TethysGizmoOptions, self).__init__()
 
         self.variables = variables
 
 
-class GridParameters(TethysGizmoOptions):
+class FetchClimateGridParameters(TethysGizmoOptions):
     """
     Grid Parameters
 
@@ -125,18 +134,19 @@ class GridParameters(TethysGizmoOptions):
         Constructor
         """
         # Initialize super class
-        super(GridParameters, self).__init__()
+        super(TethysGizmoOptions, self).__init__()
 
         self.title = title
         self.boundingBox = boundingBox
         self.gridResolution = gridResolution
 
 
-class PointParameters(TethysGizmoOptions):
+class FetchClimatePointParameters(TethysGizmoOptions):
     """
     Point Parameters
 
-    Optional if there is a map or grid included. Otherwise, it is required! No map needed. If map included, it will initialize with input point.
+    Optional if there is a map or grid included. Otherwise, it is required! No map needed.
+    If map included, it will initialize with input point.
 
     Attributes
     title(string): The name of the point location.
@@ -148,7 +158,35 @@ class PointParameters(TethysGizmoOptions):
         Constructor
         """
         # Initialize super class
-        super(PointParameters, self).__init__()
+        super(TethysGizmoOptions, self).__init__()
 
         self.title = title
         self.location = location
+
+class FetchClimateMap(TethysGizmoOptions):
+    """
+    Plot Parameters
+
+    Attributes
+    dimensions(string dictionary): The integer is in pixels for width (`Highcharts width reference
+    <http://api.highcharts.com/highcharts#chart.width>`_) or height (`Highcharts height reference
+    <http://api.highcharts.com/highcharts#chart.height>`_). Not required to be defined.
+    """
+
+    def __init__(self, url_parameter=FetchClimateURLParameter(), map_parameters=FetchClimateMapParameters(),
+                 plot_parameters=FetchClimatePlotParameters(), variable_parameters=FetchClimateVariableParameters(),
+                 grid_parameters=FetchClimateGridParameters(), point_parameters=FetchClimatePointParameters()):
+        """
+        Constructor
+        """
+        # Initialize super class
+        super(TethysGizmoOptions, self).__init__()
+
+        self.url_parameter = url_parameter
+        self.map_parameters = map_parameters
+        self.plot_parameters = plot_parameters
+        self.variable_parameters = variable_parameters
+        self.grid_parameters = grid_parameters
+        self.point_parameters = point_parameters
+
+
