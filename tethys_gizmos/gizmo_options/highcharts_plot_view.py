5b790b70792d85fb96d2461692942ca179855f17
ezrajr <ezrajrice@gmail.com>
2015-03-25 12:01:43 -0600

    #47 Making subclasses for highcharts

--- tethys_gizmos/gizmo_options/highcharts_plot_view.py
+++ tethys_gizmos/gizmo_options/highcharts_plot_view.py
@@ -1,6 +1,6 @@
 from .base import TethysGizmoOptions
 
-__all__ = ['PlotView']
+__all__ = ['PlotView', 'HighChartsObject']
 
 
 class PlotView(TethysGizmoOptions):
@@ -10,7 +10,7 @@ class PlotView(TethysGizmoOptions):
     Plot views can be used to generate interactive plots of tabular data. All of the plots available through this gizmo are powered by the Highcharts JavaScript library.
 
     Attributes
-    highcharts_object(PySON, required): The highcharts_object ocontains the definition of the chart. The full `Highcharts API reference <http://api.highcharts.com/highcharts>`_ is supported via this object. The object can either be a JavaScript string or a JavaScript-equivalent Python data structure. The latter is recommended.
+    highcharts_object(PySON, required): The highcharts_object contains the definition of the chart. The full `Highcharts API reference <http://api.highcharts.com/highcharts>`_ is supported via this object. The object can either be a JavaScript string or a JavaScript-equivalent Python data structure. The latter is recommended.
     height(string): Height of the plot element. Any valid css unit of length.
     width(string): Width of the plot element. Any valid css unit of length.
     attributes(string): Any HTML attributes to add to the plot element (e.g.: "id=foo name=bar value=hello-world")
@@ -29,16 +29,15 @@ class PlotView(TethysGizmoOptions):
         self.attributes = attributes
 
 
-
-class HighchartsObject(TethysGizmoOptions):
+class HighChartsObject(TethysGizmoOptions):
     """
-    Highcharts Object
+    HighCharts Object
 
     Attributes
     """
 
     def __init__(self, chart={}, title='', subtitle='', legend={}, xAxis={}, yAxis={}, tooltip={},
-                 series=[]):
+                 series=[], **kwargs):
         """
         Constructor
         """
@@ -53,3 +52,25 @@ class HighchartsObject(TethysGizmoOptions):
         self.yAxis = yAxis
         self.tooltip = tooltip
         self.series = series
+        #add any other attributes the user wants
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
+
+class HighChartsLinePlot(TethysGizmoOptions):
+    """
+    Line Plot
+
+    Displays as a line graph.
+
+    Attributes
+    """
+
+    def __init__(self,chart={'type': 'line'}, title='', subtitle='', legend={}, xAxis={}, yAxis={}, tooltip={},
+                 series=[]):
+        """
+        Constructor
+        """
+        # Initialize super class
+        super(HighChartsObject, self).__init__(chart=chart, title=title, subtitle=subtitle, legend=legend,
+                                               xAxis=xAxis, yAxis=yAxis, tooltip=tooltip, series=series)
